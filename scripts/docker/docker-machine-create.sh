8ee56e21e1d27f567e07489af40894f258e0c996
Ivan Paulovich <Ivan Paulovich>
2017-04-17 23:05:06 -0300

    ok

--- scripts/docker/docker-machine-create.sh
+++ scripts/docker/docker-machine-create.sh
@@ -2,3 +2,8 @@ docker-machine create \
 --driver azure \
 --azure-subscription-id 5c67839b-4422-4823-b28d-06b76002927d \
 docker-build
+
+# credenciais criadas em:
+# %userprofile%\.docker\machine\machines\docker-build
+
+docker-machine env tfs-build00
--- scripts/docker/docker-machine-ssh.sh
+++ scripts/docker/docker-machine-ssh.sh
@@ -1,1 +1,6 @@
-docker-machine ssh docker-build
+docker-machine ssh tfs-build00
+
+sudo docker run \
+  -e VSTS_ACCOUNT=ivanpaulovich \
+  -e VSTS_TOKEN=k2yaezs77yf7morjcdgxc6a6fff4lbd7cblc4pafdj7fuzkikmaq \
+  -it microsoft/vsts-agent
